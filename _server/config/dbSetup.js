b80269ecb838c94e024e37957684dd01b37dd8c1
Tyler Clark <mtclark0518@gmail.com>
2017-11-20 12:27:04 -0700

    resetting project

--- _server/config/dbSetup.js
+++
@@ -1,9 +0,0 @@
-/*
- *  Setup the db (init the tables to default/empty )
- */
-var Sequelize = require('sequelize');
-var db = require('../_model/index');
-
-db.sequelize.sync({ force: true }).then(function() {
-    process.exit();
-});
