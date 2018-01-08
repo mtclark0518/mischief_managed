8a776e9142e5bf1c551ec5d17da990b6847af8f3
mtclark0518 <mtclark0518@gmail.com>
2018-01-07 17:36:05 -0700

    comments out unused controller functions/routes

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,30 +1,24 @@
 const router = require('express').Router();
 const controller = require('../controller/controller')
 
-//CASTLE
-router.get('/api/castle', controller.showHogwarts)
 
-//HOUSE
 router.get('/api/houses', controller.getHouses)
-
-//LOCATION
 router.get('/api/locations', controller.getLocations)
-
-//STAFF
-router.get('/api/staff', controller.showStaff)
-router.get('/api/staff/location/:id', controller.staffByLocation)
-
-//STUDENT
 router.get('/api/students', controller.getStudents)
-router.get('/api/students/:id', controller.showStudent)
 router.put('/api/students/:id', controller.updateStudent)
-router.put('/api/students/hex/:id/', controller.hexStudent)
-router.put('/api/students/honor/:id', controller.honorStudent)
-router.get('/api/students/house/:id', controller.studentsByHouse)
-router.get('/api/students/location/:id', controller.studentsByLocation)
 
-//SUBJECT
-router.get('/api/subjects', controller.showSubjects)
+
+
+
+// router.get('/api/castle', controller.showHogwarts)
+// router.get('/api/staff', controller.showStaff)
+// router.get('/api/staff/location/:id', controller.staffByLocation)
+// router.get('/api/students/:id', controller.showStudent)
+// router.put('/api/students/hex/:id/', controller.hexStudent)
+// router.put('/api/students/honor/:id', controller.honorStudent)
+// router.get('/api/students/house/:id', controller.studentsByHouse)
+// router.get('/api/students/location/:id', controller.studentsByLocation)
+// router.get('/api/subjects', controller.showSubjects)
 
 
 module.exports = router;
--- backend/controller/controller.js
+++ backend/controller/controller.js
@@ -8,43 +8,6 @@ const Student = db.models.Student;
 
 
 
-//utility function that updates house points
-const updateHousePoints = houseId => {
-    House.findOne({
-        where: {id: houseId}, 
-        include: [ { model:Student }]})
-    .then(house => {
-        let students = house.Students;
-        console.log(students)
-        let individualPoints = students.map( student => {
-            return student.points;
-        })
-        let totalPoints = individualPoints.reduce((acc, cv) => acc + cv);
-        
-        house.updateAttributes({
-            points : totalPoints
-        });
-    });
-};
-
-
-
-//CASTLE - index function that returns everything
-const showHogwarts = (req, res) => {
-    Castle.findOne({ where: {id: 1},
-        include:[
-            {model: House},
-            {model: Student},
-            {model: Location},
-            {model: Staff}
-        ]
-    })
-    .then( hogwarts => { res.json(hogwarts); } );
-}
-
-
-
-
 //HOUSE
 const getHouses = (req, res) => {
     House.findAll()
@@ -75,162 +38,37 @@ const getStudents = (req, res) => {
 
 
 const updateStudent = ( req, res) => {
-    console.log(req.params.id)
-    console.log(req.body)
     let amount = req.body.amount
     Student.findOne({where: { id:req.params.id}, include:[{model: House}, {model: Location}]})
     .then( student => {
-        console.log(student.points)
         student.updateAttributes({
             points: student.points + amount
         }).then( student => {
-            console.log(student.points)
-            res.json(student)
-        })
-        // .then(student => {
-        //     updateHousePoints(student.HouseId)
-        //     res.json(student)
-        // });
-    });
-}
-
-
-
-
-//STAFF
-const showStaff = (req, res) => {
-    Staff.findAll()
-    .then( staff => {
-        res.json(staff);
-    });
-};
-//STAFF BY LOCATION
-const staffByLocation = (req, res) => {
-    Staff.findAll({
-        where: {
-            LocationId: req.params.id
-        },
-        include:[
-            {
-                model: House
-            },
-            {
-                model: Subject
-            }
-        ]
-    })
-    .then( staff => {
-        res.json(staff);
-    });
-};
-
-
-//SHOW A STUDENT
-const showStudent = (req, res) => {
-    Student.findOne({where:{id:req.params.id}, include:[{model: House}]})
-    .then( student => {
-        res.json(student);
-    });
-};
-//HEX A STUDENT
-const hexStudent = (req, res) => {
-    Student.findOne({where: { id:req.params.id} })
-    .then( student => {
-        let hex = student.points - 1;
-        student.updateAttributes({
-            points: hex
-        })
-        .then(student => {
-            updateHousePoints(student.HouseId)
             res.json(student)
         });
     });
 };
 
-//HONOR A STUDENT
-const honorStudent = (req, res) => {
-    Student.findOne({where: { id:req.params.id} })
-    .then( student => {
-        let honor = student.points + 2;
-        student.updateAttributes({
-            points: honor
-        })
-        .then(student => {
-            updateHousePoints(student.HouseId)
-            res.json(student)
-        });
-    });
-};
 
-//STUDENT BY HOUSE
-const studentsByHouse = (req, res) => {
-    Student.findAll({
-        where: {
-            HouseId: req.params.id
-        },
-        include:[
-            {
-                model: House
-            }
-        ]
-    })
-    .then( students => {
-        res.json(students);
-    });
-}
 
-//STUDENT BY LOCATION
-const studentsByLocation = (req, res) => {
-    Student.findAll({
-        where: {
-            LocationId: req.params.id
-        },
-        include:[
-            {
-                model: House
-            }
-        ]
-    })
-    .then( students => {
-        res.json(students);
-    });
-}
 
-//SUBJECT
-const showSubjects = (req, res) => {
-    Subject.findAll({
-        include: [
-            {
-                model: Staff
-            },
-            {
-                model: Location
-            }
-        ]
-    })
-    .then( subjects => {
-        res.json(subjects);
-    });
-}
 
 module.exports = { 
-    showHogwarts: showHogwarts,
     getHouses: getHouses,
     getLocations: getLocations,
     getStudents: getStudents,
-    
     updateStudent: updateStudent,
 
-
-    showStaff: showStaff,
-    staffByLocation: staffByLocation,
-    showStudent: showStudent,
-    studentsByHouse: studentsByHouse,
-    hexStudent: hexStudent,
-    honorStudent: honorStudent,
-    updateHousePoints: updateHousePoints,
-    studentsByLocation: studentsByLocation,
-    showSubjects: showSubjects,
+    // showHogwarts: showHogwarts,
+    // showStaff: showStaff,
+    // staffByLocation: staffByLocation,
+    // showStudent: showStudent,
+    // studentsByHouse: studentsByHouse,
+    // hexStudent: hexStudent,
+    // honorStudent: honorStudent,
+    // updateHousePoints: updateHousePoints,
+    // studentsByLocation: studentsByLocation,
+    // showSubjects: showSubjects,
 };
 // const login = (req, res) => {
 
@@ -305,3 +143,153 @@ module.exports = {
 //         }
 //     }).then()
 // }
+
+// //STAFF
+// const showStaff = (req, res) => {
+//     Staff.findAll()
+//     .then( staff => {
+//         res.json(staff);
+//     });
+// };
+// //STAFF BY LOCATION
+// const staffByLocation = (req, res) => {
+//     Staff.findAll({
+//         where: {
+//             LocationId: req.params.id
+//         },
+//         include:[
+//             {
+//                 model: House
+//             },
+//             {
+//                 model: Subject
+//             }
+//         ]
+//     })
+//     .then( staff => {
+//         res.json(staff);
+//     });
+// };
+
+
+// //SHOW A STUDENT
+// const showStudent = (req, res) => {
+//     Student.findOne({where:{id:req.params.id}, include:[{model: House}]})
+//     .then( student => {
+//         res.json(student);
+//     });
+// };
+// //HEX A STUDENT
+// const hexStudent = (req, res) => {
+//     Student.findOne({where: { id:req.params.id} })
+//     .then( student => {
+//         let hex = student.points - 1;
+//         student.updateAttributes({
+//             points: hex
+//         })
+//         .then(student => {
+//             updateHousePoints(student.HouseId)
+//             res.json(student)
+//         });
+//     });
+// };
+
+// //HONOR A STUDENT
+// const honorStudent = (req, res) => {
+//     Student.findOne({where: { id:req.params.id} })
+//     .then( student => {
+//         let honor = student.points + 2;
+//         student.updateAttributes({
+//             points: honor
+//         })
+//         .then(student => {
+//             updateHousePoints(student.HouseId)
+//             res.json(student)
+//         });
+//     });
+// };
+
+// //STUDENT BY HOUSE
+// const studentsByHouse = (req, res) => {
+//     Student.findAll({
+//         where: {
+//             HouseId: req.params.id
+//         },
+//         include:[
+//             {
+//                 model: House
+//             }
+//         ]
+//     })
+//     .then( students => {
+//         res.json(students);
+//     });
+// }
+
+// //STUDENT BY LOCATION
+// const studentsByLocation = (req, res) => {
+//     Student.findAll({
+//         where: {
+//             LocationId: req.params.id
+//         },
+//         include:[
+//             {
+//                 model: House
+//             }
+//         ]
+//     })
+//     .then( students => {
+//         res.json(students);
+//     });
+// }
+
+// //SUBJECT
+// const showSubjects = (req, res) => {
+//     Subject.findAll({
+//         include: [
+//             {
+//                 model: Staff
+//             },
+//             {
+//                 model: Location
+//             }
+//         ]
+//     })
+//     .then( subjects => {
+//         res.json(subjects);
+//     });
+// }
+// //utility function that updates house points
+// const updateHousePoints = houseId => {
+//     House.findOne({
+//         where: {id: houseId}, 
+//         include: [ { model:Student }]})
+//     .then(house => {
+//         let students = house.Students;
+//         console.log(students)
+//         let individualPoints = students.map( student => {
+//             return student.points;
+//         })
+//         let totalPoints = individualPoints.reduce((acc, cv) => acc + cv);
+        
+//         house.updateAttributes({
+//             points : totalPoints
+//         });
+//     });
+// };
+
+
+
+// //CASTLE - index function that returns everything
+// const showHogwarts = (req, res) => {
+//     Castle.findOne({ where: {id: 1},
+//         include:[
+//             {model: House},
+//             {model: Student},
+//             {model: Location},
+//             {model: Staff}
+//         ]
+//     })
+//     .then( hogwarts => { res.json(hogwarts); } );
+// }
+
