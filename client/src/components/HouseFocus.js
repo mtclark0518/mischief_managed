3529fb34651f4d2c1c4e133250c839fbe0e13121
mtclark0518 <mtclark0518@gmail.com>
2018-01-15 19:06:14 -0700

    style

--- client/src/components/HouseFocus.js
+++ client/src/components/HouseFocus.js
@@ -6,10 +6,16 @@ const HouseFocus = props => {
         console.log(event.target)
         props.focus(event.target.id)
     }
+
     const data = props.content.map(data => {
+        let style = {
+            boxShadow: '0 0 1px 0 ' + data.props.colors.primary,
+            color: data.props.colors.primary
+        }
         return(
             <button 
                 key={'ff'+data.key}
+                style={style}
                 id={data.props.id}
                 className="secondaryItem"
                 onClick={e => giveFocus(e)}>
