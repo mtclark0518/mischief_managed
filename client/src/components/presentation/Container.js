aae4d38f23c6afaf250929cc9c0bcddae6de2422
mtclark0518 <mtclark0518@gmail.com>
2018-01-22 20:36:04 -0700

    adds initial interaction function

--- client/src/components/presentation/Container.js
+++ client/src/components/presentation/Container.js
@@ -4,21 +4,75 @@ import Student from '../stateful/Student'
 import Staff from './Staff'
 class Container extends Component {
 
-
-
-
     render(){
-
-
 
         const people = {
             students: this.props.students,
             staff: this.props.staff
         }
+
         const interaction = (one, two) => {
-            console.log(one, two)
-            console.log(people.staff)
-        }
+
+            people.staff.forEach( person => {
+                let bias = !person.House? 'none' : person.House.name;
+                person.bias = bias
+                let through = {
+                    id: person.id,
+                    bias: bias,
+                    awareness: parseFloat(person.awareness),
+                    severity: parseFloat(person.severity)
+                }
+                let from = people.students.filter( student => student.id == one).map(student => {
+                    return {
+                        id: student.id,
+                        bias: student.House.name
+                    }
+                })
+                let to = people.students.filter( student => student.id == two).map(student => {
+                    return {
+                        id: student.id,
+                        bias: student.House.name
+                    }
+                })
+                let action = {
+                    from: from[0],
+                    to: to[0],
+                    through: through
+                }
+                let awareness = action.through.awareness; 
+
+                const assignBias = () => {
+                    if(action.from.bias == action.through.bias) {
+                        awareness += 0.15
+                    } else if(action.to.bias == action.through.bias) { 
+                        awareness -= 0.15
+                    }
+                    processAttempt(awareness)
+                }
+                const processAttempt = odds => {
+                    let chance = Math.random()
+                    chance > odds ? successResponse() : failureResponse()
+                }
+
+                const failureResponse = () => {
+                    
+                    console.log('this is the fucking failure response')
+                    let impact = parseInt((-10 * action.through.severity)/2);
+                    console.log(impact)
+                    this.props.sendUpdate(action.from.id, impact )
+                }
+                const successResponse = () => {
+                    
+                    console.log('this is the fucking succccceeesss')
+                    this.props.sendUpdate(action.from.id, 15);
+                    this.props.sendUpdate(action.to.id, -3);
+                };
+
+                assignBias();
+            });
+
+        };
+
 
         const students = this.props.students.map( student => {
             return(
@@ -68,9 +122,8 @@ class Container extends Component {
                 )}
             </div>
         )
+
+
     }  
-    interaction = (people,staff) => {
-        console.log(people)
-    }
 }
 export default Container;
